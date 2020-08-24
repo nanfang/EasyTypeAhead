731eaaecd11f0a6f5b87e4e8930530032ea8e85f
Fang Nan <nanfang05@gmail.com>
2020-08-24 10:42:40 -0700

    Enable the service (MVP)

--- easytypeahead/core/search_tree.py
+++ easytypeahead/core/search_tree.py
@@ -9,16 +9,18 @@ class Node:
         self.children = defaultdict(Node)
 
 
-class TrieTreeSearcher:
+class TrieTreeIndex:
 
     def __init__(self, root=None, top_limit=10):
         self.root = root or Node()
         self.top_limit = top_limit
 
     def search(self, prefix: str) -> list:
+        if not prefix:
+            return []
         current = self.root
         for c in prefix:
-            if c in current.chrilden:
+            if c in current.children:
                 current = current.children[c]
             else:
                 return []
@@ -29,15 +31,16 @@ class TrieTreeSearcher:
         for c in term:
             current = current.children[c]
         current.term = term
-        current.count = count
+        current.search_count = count
 
     def build_tops(self) -> None:
         def dfs(node):
             node.tops.clear()
-            for child_node in node.children.values:
+            if node.term:
+                node.tops.append(node)
+            for child_node in node.children.values():
                 node.tops.extend(dfs(child_node))
             node.tops.sort(key=lambda n: n.search_count, reverse=True)
             del node.tops[self.top_limit:]
             return node.tops
-
         dfs(self.root)
--- easytypeahead/flaskr/app.py
+++ easytypeahead/flaskr/app.py
@@ -1,20 +1,41 @@
-import json
-
 from flask import Flask
 from flask import request
+from flask import jsonify
 
-app = Flask(__name__)
+from easytypeahead.core.search_tree import TrieTreeIndex
+
+
+def init_index():
+    print('init the Trie Tree index')
+    index_tree = TrieTreeIndex()
+    # FIXME: load from storage
+    index_tree.add_term('abc', 10)
+    index_tree.add_term('abstract', 5)
+    index_tree.add_term('apple', 100)
+    index_tree.add_term('alpine', 98)
+    index_tree.add_term('app', 1000)
+    index_tree.add_term('application', 99)
+    index_tree.build_tops()
+    return index_tree
+
+
+def create_app():
+    return Flask(__name__), init_index()
+
+
+app, index = create_app()
 
 
 @app.route('/')
 def default():
-    return 'Easy TypeAhead is alive!'
+    return 'Easy Type Ahead is alive!'
 
 
+# /suggest?t={search_term}
 @app.route('/suggest')
 def suggest():
-    term = request.args.get('term', '')
-
-    print('search:%s' % term)
-    # TODO use search tree to search
-    return json.dumps(['ab', 'abc', 'abcd'])
+    search_term = request.args.get('t', '')
+    search_term = search_term.strip()
+    print('search:%s' % search_term)
+    result = index.search(search_term)
+    return jsonify(result)
